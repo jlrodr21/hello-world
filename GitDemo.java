0e06a5618685688fe3f86feb75463cfa16e89ea9
jlrodr21 <79817446+jlrodr21@users.noreply.github.com>
2021-02-28 21:12:55 -0700

    Swaping code
    The codes of methods reverseNumber() and findFactorial() were swapped

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Jorge Rodriguez.
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,8 +14,10 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        double sum;
         System.out.print("Add num to previousNumber");
-        return 0;
+        sum = previousNumber + num;
+        return sum;
     }
     
     /*****
@@ -23,6 +26,23 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
+        double newNumber = 1;
+        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }
+        
+        reverseNum = newNumber;
+        
+        return newNumber;
+    }
+    
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
         double factorialNumber = 0;
         
         while( num != 0 ) {
@@ -32,21 +52,6 @@ public class GitDemo {
         }
         
         return factorialNumber;
-    }
-    
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
-        }
-        
-        return newNumber;
     }
     
 }
